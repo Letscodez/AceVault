b74f0e90157b08e7dd5dfe8d16c2ed6b95e946c1
Anuj Sah <letscodez17@gmail.com>
2024-11-29 19:56:09 +0530

    search mechanism

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -57,24 +57,46 @@ export const uploadFile = async ({
     handleError(error, "Error uploading file");
   }
 };
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (
+  currentUser: Models.Document,
+  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number,
+) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
       Query.contains("users", [currentUser.$id]),
     ]),
   ];
+
+  if (types.length > 0) queries.push(Query.equal("type", types));
+  if (searchText) queries.push(Query.contains("name", searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  if (sort) {
+    const [sortBy, orderBy] = sort.split("-");
+    queries.push(
+      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy),
+    );
+  }
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({
+  types = [],
+  searchText = "",
+  sort = "$createdAt-desc",
+  limit,
+}: GetFilesProps) => {
   const { databases } = await createAdminClient();
 
   try {
     const currentUser = await getCurrentUser();
 
     if (!currentUser) throw new Error("User not found");
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
 
     // List documents using queries
     const files = await databases.listDocuments(
@@ -88,6 +110,29 @@ export const getFiles = async () => {
   }
 };
 
+export const renameFile = async ({
+  fileId,
+  name,
+  extension,
+  path,
+}: RenameFileProps) => {
+  const { databases } = await createAdminClient();
+  try {
+    const newName = `${name}.${extension}`;
+    const updatedFile = await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      fileId,
+      {
+        name: newName,
+      },
+    );
+    revalidatePath(path);
+    return parseStringify(updatedFile);
+  } catch (error) {
+    handleError(error, "Error renaming file");
+  }
+};
 export const updateFileUsers = async ({
   fileId,
   emails,
@@ -109,25 +154,23 @@ export const updateFileUsers = async ({
     handleError(error, "Error renaming file");
   }
 };
-export const renameFile = async ({
+export const deleteFile = async ({
   fileId,
-  name,
-  extension,
+  bucketFileId,
   path,
-}: RenameFileProps) => {
-  const { databases } = await createAdminClient();
+}: DeleteFileProps) => {
+  const { databases, storage } = await createAdminClient();
   try {
-    const newName = `${name}.${extension}`;
-    const updatedFile = await databases.updateDocument(
+    const deletedFile = await databases.deleteDocument(
       appwriteConfig.databaseId,
       appwriteConfig.filesCollectionId,
       fileId,
-      {
-        name: newName,
-      },
     );
+    if (deletedFile) {
+      await storage.deleteFile(appwriteConfig.bucketId, bucketFileId);
+    }
     revalidatePath(path);
-    return parseStringify(updatedFile);
+    return parseStringify({ status: "success" });
   } catch (error) {
     handleError(error, "Error renaming file");
   }
